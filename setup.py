729b2a7955f11270d223a12fa6bad7ad8c252a12
Thomas Weissgerber <thomas.weissgerber97@gmail.com>
2025-10-25 12:23:00 +0000

    feat: add project dependencies requirement

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='unifi-hotspot-logger',
+      version='0.1',
+      description='Simple webhook service that can log data coming from the unifi-hotspot app.',
+      url='https://github.com/Thowaah/unifi-hotspot-logger',
+      author='Thomas Weissgerber',
+      author_email='thomas.weissgerber97@gmail.com',
+      install_requires=[
+          'flask',
+      ],
+      zip_safe=False)
